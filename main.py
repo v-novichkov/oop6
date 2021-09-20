3e155b06dbdac470f03d99dbde43602230d01217
Vladislav Novichkov <novichkov.vr@gmail.com>
2021-09-20 19:39:11 +0700

    Rate mentors added & Reviewer rate HW

--- main.py
+++ main.py
@@ -7,14 +7,28 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_mentors(self, mentor, course, grade):
+        if isinstance(mentor, Mentor) and course in mentor.courses_attached:
+            if course in mentor.mentors_grades:
+                mentor.mentors_grades[course] += [grade]
+            else:
+                mentor.mentors_grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+        self.mentors_grades = {}
 
-    def rate_hw(self, student, course, grade):
+
+class Lecturer(Mentor):
+    pass
+
+class Reviewer(Mentor):
+     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
@@ -22,12 +36,6 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
-class Lecturer(Mentor):
-    pass
-
-class Reviewer(Mentor):
-    pass
 
 
 
